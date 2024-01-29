ada5a7afe31f4ae9b3e0e33097ffac6062d2d903
Chris <christopherdchan02@gmail.com>
2024-01-28 22:23:03 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -1,39 +1,94 @@
-class X:
+class Expression:
+    def __init__(self, tier, op=None):
+        self.tier = tier
+        self.op = op
+
+    def __repr__(self):
+        if self.p1.tier < self.tier:
+            left = "( " + repr(self.p1) + " )"
+        else:
+            left = repr(self.p1)
+        
+        if self.p2.tier < self.tier:
+            right = "( " + repr(self.p2) + " )"
+        else:
+            right = repr(self.p2)
+
+        return left + " " + self.op + " " + right
+
+class X(Expression):
     def __init__(self):
-        pass
+        super().__init__(3)
 
     def __repr__(self):
         return "X"
 
-class Int:
+class Int(Expression):
     def __init__(self, i):
+        super().__init__(3)
         self.i = i
     
     def __repr__(self):
         return str(self.i)
 
-class Add:
+
+class Add(Expression):
     def __init__(self, p1, p2):
+        super().__init__(1, "+")
         self.p1 = p1
         self.p2 = p2
-    
-    def __repr__(self):
-        return repr(self.p1) + " + " + repr(self.p2)
 
-class Mul:
+class Sub(Expression):
     def __init__(self, p1, p2):
+        super().__init__(1, "-")
         self.p1 = p1
         self.p2 = p2
-    
-    def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " * " + repr(self.p2)
+
+class Mul(Expression):
+    def __init__(self, p1, p2):
+        super().__init__(2, "*")
+        self.p1 = p1
+        self.p2 = p2
+
+class Div(Expression):
+    def __init__(self, p1, p2):
+        super().__init__(2, "/")
+        self.p1 = p1
+        self.p2 = p2
 
 
-poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+def test_given_test():
+    poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+    answer = "4 + 3 + X + 1 * ( X * X + 1 )"
+
+    assert repr(poly) == answer
+
+def test_mul():
+    poly = Mul( X(), Int(5) )
+    answer = "X * 5"
+
+    assert repr(poly) == answer
+
+def test_div():
+    poly = Div( X(), Int(5) )
+    answer = "X / 5"
+
+    assert repr(poly) == answer
+
+def test_all_add():
+    poly = Add( Add ( Add ( Int(4), Int(5) ), X() ), Add( Int(5), Int(10)))
+    answer = "4 + 5 + X + 5 + 10"
+
+    assert repr(poly) == answer
+
+def test_alternate_three_times():
+    poly = Add( Div( Add( Int(5), Int(3) ), Add( Int(7), Int(9) ) ), Int(3))
+    answer = "( 5 + 3 ) / ( 7 + 9 ) + 3"
+
+    assert repr(poly) == answer
+
+def test_all_four():
+    poly = Add( Div( Int(4), Sub( Int(3), X() )), Mul( Int(3), Int(8)))
+    answer = "4 / ( 3 - X ) + 3 * 8"
+
+    assert repr(poly) == answer
