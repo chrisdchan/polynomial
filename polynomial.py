85a4aea35f869ef79a660d7bfda8733ef54be039
Chris <christopherdchan02@gmail.com>
2024-01-28 22:41:25 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -1,3 +1,5 @@
+import pytest
+
 class Expression:
     def __init__(self, tier, op=None):
         self.tier = tier
@@ -23,6 +25,9 @@ class X(Expression):
     def __repr__(self):
         return "X"
 
+    def evaluate(self, x):
+        return x
+
 class Int(Expression):
     def __init__(self, i):
         super().__init__(3)
@@ -31,6 +36,9 @@ class Int(Expression):
     def __repr__(self):
         return str(self.i)
 
+    def evaluate(self, _):
+        return self.i
+
 
 class Add(Expression):
     def __init__(self, p1, p2):
@@ -38,11 +46,17 @@ class Add(Expression):
         self.p1 = p1
         self.p2 = p2
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
+
 class Sub(Expression):
     def __init__(self, p1, p2):
         super().__init__(1, "-")
         self.p1 = p1
         self.p2 = p2
+
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
 
 class Mul(Expression):
     def __init__(self, p1, p2):
@@ -50,44 +64,70 @@ class Mul(Expression):
         self.p1 = p1
         self.p2 = p2
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
+
 class Div(Expression):
     def __init__(self, p1, p2):
         super().__init__(2, "/")
         self.p1 = p1
         self.p2 = p2
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
 
-def test_given_test():
+def test_eval_add():
+    poly = Add(Int(5), X())
+    actual = poly.evaluate(2)
+    expected = 7
+
+    assert actual == expected
+
+def test_eval_given():
+    poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+    actual = poly.evaluate(-1)
+    expected = 8
+
+    assert actual == expected
+
+def test_divide_by_zero():
+    poly = Div( Int(10), Sub(X(), X()))
+    
+    with pytest.raises(ZeroDivisionError):
+        poly.evaluate(10)
+
+
+def test_repr_given_test_repr():
     poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
     answer = "4 + 3 + X + 1 * ( X * X + 1 )"
 
     assert repr(poly) == answer
 
-def test_mul():
+def test_repr_mul():
     poly = Mul( X(), Int(5) )
     answer = "X * 5"
 
     assert repr(poly) == answer
 
-def test_div():
+def test_repr_div():
     poly = Div( X(), Int(5) )
     answer = "X / 5"
 
     assert repr(poly) == answer
 
-def test_all_add():
+def test_repr_all_add():
     poly = Add( Add ( Add ( Int(4), Int(5) ), X() ), Add( Int(5), Int(10)))
     answer = "4 + 5 + X + 5 + 10"
 
     assert repr(poly) == answer
 
-def test_alternate_three_times():
+def test_repr_alternate_three_times():
     poly = Add( Div( Add( Int(5), Int(3) ), Add( Int(7), Int(9) ) ), Int(3))
     answer = "( 5 + 3 ) / ( 7 + 9 ) + 3"
 
     assert repr(poly) == answer
 
-def test_all_four():
+def test_repr_all_four():
     poly = Add( Div( Int(4), Sub( Int(3), X() )), Mul( Int(3), Int(8)))
     answer = "4 / ( 3 - X ) + 3 * 8"
 
